1b2b0d99f6a5c233a08d8b62122615efc4419721
AnastasiaShade <anstasiashade@gmail.com>
2018-03-02 12:32:56 +0300

    Добавлена лабораторная работа №1

---
+++ src/Backend/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        static readonly ConcurrentDictionary<string, string> _data = new ConcurrentDictionary<string, string>();
+
+        // GET api/values/<id>
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            string value = null;
+            _data.TryGetValue(id, out value);
+            return value;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public string Post([FromForm]string value)
+        {
+            var id = Guid.NewGuid().ToString();
+            _data[id] = value;
+            return id;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
