c56cbaaffd6cad79f46905c1944ad6e4bcd09800
AnastasiaShade <anstasiashade@gmail.com>
2018-06-08 15:51:34 +0300

    Инициализирована ЛР 6

---
+++ lw6/build.cmd
@@ -0,0 +1,38 @@
+@echo off
+
+if "%~1" EQU "" goto error
+
+echo Creating project structure...
+mkdir lw6_%~1\"Frontend"
+mkdir lw6_%~1\"Backend"
+mkdir lw5_%~1\"TextListener"
+mkdir lw5_%~1\"TextRankCalc"
+mkdir lw6_%~1\"VowelConsCounter"
+mkdir lw6_%~1\"VowelConsRater"
+mkdir lw6_%~1"\"config"
+
+echo Compiling project...
+start /wait /d src\Backend dotnet publish
+start /wait /d src\Frontend dotnet publish
+start /wait /d src\TextListener dotnet publish
+start /wait /d src\TextRankCalc dotnet publish
+start /wait /d src\VowelConsCounter dotnet publish
+start /wait /d src\VowelConsRater dotnet publish
+
+echo Copying files...
+xcopy src\Frontend\bin\Debug\netcoreapp2.0\publish lw6_"%~1"\"Frontend"
+xcopy src\Backend\bin\Debug\netcoreapp2.0\publish lw6_"%~1"\"Backend"
+xcopy src\TextListener\bin\Debug\netcoreapp2.0\publish lw6_"%~1"\"TextListener"
+xcopy src\TextRankCalc\bin\Debug\netcoreapp2.0\publish lw6_"%~1"\"TextRankCalc"
+xcopy src\VowelConsCounter\bin\Debug\netcoreapp2.0\publish lw6_"%~1"\"VowelConsCounter"
+xcopy src\VowelConsRater\bin\Debug\netcoreapp2.0\publish lw6_"%~1"\"VowelConsRater"
+xcopy config lw6_"%~1"\config
+xcopy run.cmd lw6_"%~1"
+xcopy stop.cmd lw6_"%~1"
+
+echo Success!
+exit 0
+
+:error
+echo Invalid arguments count!
+exit 1
---
+++ lw6/src/Backend/Controllers/ValuesController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using StackExchange.Redis;
+using System.Threading;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private string GetFromDB(string id)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost, abortConnect=false");
+            IDatabase db = redis.GetDatabase();
+            string value = db.StringGet(id);
+            int tryNumber = 10;
+            int sleepTime = 200;
+
+            for (int i = 0; i <= tryNumber; ++i)
+            {
+                if (value != null)
+                {
+                    try
+                    {
+                        float tmpValue = (float) Convert.ToDouble(value);
+                        break;
+                    }
+                    catch(FormatException)
+                    {
+
+                    }
+                }
+
+                Thread.Sleep(sleepTime);
+                value = db.StringGet(id);
+            }
+
+            return value;
+        }
+
+        private IActionResult GetStatusCode(string value)
+        {
+            IActionResult result = null;
+            if (value != null) 
+            {
+                result = Ok(value);
+            }
+            else
+            {
+                result = new NotFoundResult();
+            }
+            return result;
+        }
+
+        // GET api/values/<id>
+        [HttpGet("{id}")]
+        public IActionResult Get(string id)
+        {
+            string value = GetFromDB(id);
+            return GetStatusCode(value);
+        }
+
+        private void SendToBD(string id, string value)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost, abortConnect=false");
+            IDatabase db = redis.GetDatabase();
+
+            db.StringSet(id, value);
+        }
+
+        private void SendToQueue(string id)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("backend-api", "fanout");
+
+                string message = "TextCreated:" + id;
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "backend-api",
+                                    routingKey: "",
+                                    basicProperties: null,
+                                    body: body
+                ); 
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public string Post([FromForm] string value)
+        {         
+            var id = Guid.NewGuid().ToString();
+            SendToBD(id, value);
+            SendToQueue(id);
+            return id;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
